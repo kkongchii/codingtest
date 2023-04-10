2a6317a30d3e65cbb821553cf432fc54c92139e9
kkongchii <90906863+kkongchii@users.noreply.github.com>
2023-04-10 19:36:00 +0900

    [level 1] Title: 폰켓몬, Time: 0.19 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -0,0 +1,7 @@
+def solution(nums):
+    set_poketmon = set(nums)
+    if len(set_poketmon) > len(nums)/2:
+        answer = len(nums)/2
+    else:
+        answer = len(set_poketmon)
+    return answer
