adf4680f14fb2f3631e7d312695afec7a4794493
kkongchii <90906863+kkongchii@users.noreply.github.com>
2023-05-01 14:49:31 +0900

    [level 2] Title: 주식가격, Time: 102.61 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -1,17 +1,12 @@
 
+
 def solution(prices):
-    answer = [0]*len(prices)
-    stack = []
-    
-    for i, price in enumerate(prices):
-        while stack and price < prices[stack[-1]]:
-            j = stack.pop()
-            answer[j] = i - j
-        stack.append(i)
-
-    # for문 다 돌고 Stack에 남아있는 값들 pop
-    while stack:
-        j = stack.pop()
-        answer[j] = len(prices) - 1 - j
-
+    answer =[0] * len(prices)
+    for i in range(len(prices)):
+        for j in range(i+1, len(prices)):
+            if prices[i] <= prices[j]:
+                answer[i]+=1
+            else:
+                answer[i]+=1
+                break
     return answer
